ce9124dbf2af9c02a32a973250fbf6a8e2baf2a4
josepk <josepk@gmail.com>
2013-07-11 20:24:50 +0000

    web.js server html index added

--- web.js
+++ web.js
@@ -1,4 +1,4 @@
-var express = require('express');
+/*var express = require('express');
 
 var app = express.createServer(express.logger());
 
@@ -10,3 +10,23 @@ var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+*/
+
+var http = require('http'),
+    fs = require('fs'),
+    util = require('util'),
+    url = require('url');
+
+var server = http.createServer(function(req, res) {
+    if(url.parse(req.url).pathname == '/') {
+        res.writeHead(200, {'content-type': 'text/html'});
+        var rs = fs.createReadStream('index.html');
+        util.pump(rs, res);
+    } else {
+        res.writeHead(404, {'content-type': 'text/html'});
+        var rs = fs.createReadStream('404.html');
+        util.pump(rs, res);
+    }
+});
+
+server.listen(8080);
